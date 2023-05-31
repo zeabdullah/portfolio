69c376a3732bb56b3679ac2f514fe5b26b83a077
zeabdullah <abdullahzeidan@hotmail.com>
2023-05-31 21:19:53 +0300

    fix: 🧪 bug with attractive prop in BtnLink
    link used to show light text over light background when app is in dark mode

--- src/components/BtnLink.tsx
+++ src/components/BtnLink.tsx
@@ -12,18 +12,18 @@ export default function BtnLink({
 }: BtnLinkProps) {
     const linkBtnCls = clsx(
         'flex justify-center items-center gap-2',
-        'font-bold dark:text-light',
+        'font-bold',
         attractive
             ? 'bg-dark text-light dark:bg-neutral-100 dark:text-dark ' +
                   'hover:bg-neutral-100 hover:text-dark focus:bg-neutral-100 focus:text-dark ' +
                   'dark:hover:bg-dark dark:hover:text-light dark:focus:bg-dark dark:focus:text-light'
-            : 'bg-neutral-100 dark:bg-dark ' +
+            : 'bg-neutral-100 dark:bg-dark dark:text-light ' +
                   'hover:bg-neutral-200 focus:bg-neutral-200 ' +
                   'dark:hover:bg-neutral-800 dark:focus:bg-neutral-800',
 
         'border border-neutral-300 hover:border-neutral-400 dark:border-opacity-50',
         'rounded-lg py-2 px-4',
-        'transition-[background-color]',
+        'transition-colors',
     )
 
     return (
