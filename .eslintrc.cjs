febe9f5bd44e9573734d84bb009984670b8d60b8
zeabdullah <abdullahzeidan@hotmail.com>
2023-06-10 13:04:16 +0300

    build: 📦 re-add eslint-plugin-jsx-a11y

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jsx-a11y/recommended',
+    ],
+    rules: {
+        '@typescript-eslint/consistent-type-imports': [
+            'warn',
+            {
+                prefer: 'type-imports',
+                fixStyle: 'inline-type-imports',
+            },
+        ],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_' },
+        ],
+    },
+}
+
+module.exports = config
