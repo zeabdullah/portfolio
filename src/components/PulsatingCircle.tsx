7ecdbf98dd24f2f0517e187a4c0c5a464d238e18
zeabdullah <abdullahzeidan@hotmail.com>
2023-06-17 20:16:08 +0300

    chore: slightly adjust style of PulsatingCircle

--- src/components/PulsatingCircle.tsx
+++ src/components/PulsatingCircle.tsx
@@ -1,7 +1,7 @@
 export default function PulsatingCircle() {
     return (
-        <span className='absolute end-0 top-0 aspect-square h-2 rounded-full bg-violet-600 dark:bg-yellow-500'>
-            <span className='absolute h-full w-full animate-ping rounded-full bg-inherit' />
+        <span className='absolute -start-0.5 -top-0.5 aspect-square h-2.5 rounded-full bg-yellow-600 dark:bg-yellow-500 md:-end-0.5 md:start-auto'>
+            <span className='absolute h-full w-full animate-ping rounded-full bg-inherit opacity-80' />
         </span>
     )
 }
