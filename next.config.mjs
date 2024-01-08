981f86cde5433920adfd87aa5314e76b7303e7c8
Abdullah Z <business@zabd.dev>
2024-01-08 16:10:15 +0200

    build: remove unused mdx packages
    * including contentlayer

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import createMDX from '@next/mdx'
-
-const withMDX = createMDX({
-    extension: /\.mdx?$/, // allow both .md and .mdx
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 }
 
-export default withMDX(nextConfig)
+export default nextConfig
