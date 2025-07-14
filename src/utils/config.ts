24ef8dbbbd1a79fe67836927897b56c0a9aeb867
Abdullah Z <58212246+zeabdullah@users.noreply.github.com>
2025-07-14 16:02:50 +0300

    chore: update email

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const GITHUB_PROFILE_URL = 'https://github.com/zeabdullah'
 export const LINKEDIN_PROFILE_URL = 'https://linkedin.com/in/zeabdullah'
-export const MY_EMAIL = 'contact@zabd.dev'
+export const MY_EMAIL = 'me@zabd.dev'
 export const MAIL_TO_ME = `mailto:${MY_EMAIL}`
 export const RESUME_REMOTE_URL =
     'https://drive.google.com/file/d/1F46afVwdRy_cAWP1FPXgCc2enqv-0rQp/view?usp=sharing'
